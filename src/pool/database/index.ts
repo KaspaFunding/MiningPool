f69a190889369be9cc6d7bc2356d7020aff1b49f
KaspaNinja <153366933+LiveLaughLove13@users.noreply.github.com>
2025-05-09 00:36:43 -0400

    Update index.ts

--- src/pool/database/index.ts
+++ src/pool/database/index.ts
@@ -4,6 +4,18 @@ type Miner = {
   balance: bigint
 }
 
+type Payout = {
+  address: string
+  amount: bigint
+  txid: string
+  timestamp: number
+}
+
+type HashratePoint = {
+  timestamp: number
+  hashrate: number
+}
+
 const defaultMiner: Miner = {
   balance: 0n
 }
@@ -11,12 +23,16 @@ const defaultMiner: Miner = {
 export default class Database {
   db: RootDatabase<any, Key>
   miners: SubDatabase<Miner, string>
+  payouts: SubDatabase<Payout[], string>
+  hashrateHistory: SubDatabase<HashratePoint[], string>
 
   constructor (path: string) {
     this.db = open({
       path: path
     })
     this.miners = this.db.openDB('miners', {})
+    this.payouts = this.db.openDB('payouts', {})
+    this.hashrateHistory = this.db.openDB('hashrate', {})
   }
 
   getMiner (address: string) {
@@ -29,6 +45,35 @@ export default class Database {
       miner.balance += balance
 
       this.miners.putSync(address, miner)
+    })
+  }
+
+  getRecentPayouts() {
+    return this.payouts.get('recent') ?? []
+  }
+
+  getHashrateHistory() {
+    return this.hashrateHistory.get('history') ?? []
+  }
+
+  recordPayout(payout: Payout) {
+    return this.payouts.transactionSync(() => {
+      const recent = this.getRecentPayouts()
+      recent.unshift(payout)
+      if (recent.length > 100) recent.pop()
+      this.payouts.putSync('recent', recent)
+    })
+  }
+
+  recordHashrate(hashrate: number) {
+    return this.hashrateHistory.transactionSync(() => {
+      const history = this.getHashrateHistory()
+      history.push({
+        timestamp: Date.now(),
+        hashrate
+      })
+      if (history.length > 100) history.shift()
+      this.hashrateHistory.putSync('history', history)
     })
   }
 }
